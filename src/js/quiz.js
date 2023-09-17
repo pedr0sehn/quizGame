486f680e9c933b66b8947da4f8442550a163f61d
Pedro Sehn <pedroarthursehn@gmail.com>
2023-09-17 19:31:19 -0300

    last commit

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,14 +1,10 @@
 var body = document.body
+
+var divStart = document.querySelector('.divStart')
 var startQuizButton = document.querySelector('.startQuizButton')
-
 // TO DO: make the credits button works
+var credits = document.querySelector('.credits')
 
-var credits = document.createElement('div')
-credits.classList.add('coolButtonTemplate')
-credits.textContent = "Créditos"
-setTimeout(() => {
-    body.appendChild(credits)
-}, 500)
 
 var multichoice
 var trueorfalse
@@ -20,11 +16,11 @@ var correctAnswers = 0
     the penult is a variable called "trueorfalse" or "multichoise" these variables are just there for indentify what is the type of the question
 */
 
-var question1 = ["2 + 2 is equal to:", "A) 22", "B) 44", "C) 4", "D) 0", multichoice, "c"]
-var question2 = ["2 is even", "True", "False", trueorfalse, "False"]
-var question3 = ["25 * 5 is equal to 125", "True", "False", trueorfalse, "False"]
-var question5 = ["What are the first three digits of PI?", "3.14", "3.05", "3.33", "4.25", multichoice, "a"]
-var question4 = ["15 / 3 is equal to 3", "True", "False", trueorfalse, "True"]
+var question1 = ["2 + 2 is equal to:", "a) 22", "b) 44", "c) 4", "d) 0", multichoice, "c"]
+var question2 = ["2 is even", "True", "False", trueorfalse, "True"]
+var question3 = ["25 * 5 is equal to 125", "True", "False", trueorfalse, "True"]
+var question5 = ["What are the first three digits of PI?", "a) 3.14", "b) 3.05", "c) 3.33", "d) 4.25", multichoice, "a"]
+var question4 = ["15 / 3 is equal to 3", "True", "False", trueorfalse, "False"]
 var question6 = ["The square root of 121 is:", "A) Doesn't exist", "B) 11", "C) 12", "D) 9", multichoice, "b"]
 var question7 = ["60 / 3 * 5 is equal to:", "A) 20", "B) 15", "C) 100", "D) 4", multichoice, "d"]
 var question8 = ["Exist negatives square roots", "True", "False", trueorfalse, "False"]
@@ -36,9 +32,6 @@ var questions = [question1, question2, question3, question4, question5, question
 var currentNumber = 0
 var pastCurrentNumber = currentNumber - 1
 
-// TO DO: I need make something to currentQuestion be relative. im getting issues by passing currentNumber = 0 and the currentQuestion doesn't update his question[currentNumber]
-
-
 function createQ() {
     if (currentNumber >= 0 && currentNumber < questions.length) {
 
@@ -48,14 +41,16 @@ function createQ() {
         clickState = false
 
         var checkAnswer = () => {
-            if (currentQuestion.includes(multichoice)) {
-                return 4
-            } else {
-                return 2
+            let typeQuestion
+            if (currentQuestion.length == 5) {
+                typeQuestion = "trueorfalse"
+            } else if (currentQuestion.length == 7) {
+                typeQuestion = "multichoice"
             }
+            return typeQuestion
         }
 
-        if (checkAnswer() == 4) {
+        if (checkAnswer() == "multichoice") {
             createQuestion.classList.add('question')
             createQuestion.innerText = `${ask}`
 
@@ -79,7 +74,7 @@ function createQ() {
             let answer = [answer1, answer2, answer3, answer4]
             let answerText = [answerText1, answerText2, answerText3, answerText4]
 
-            for (let i = 0; i < checkAnswer(); i++) {
+            for (let i = 0; i < 4; i++) {
                 let j = "abcd"
                 answer[i].classList.add(`${j[i]}`)
                 answerText[i].classList.add(`${j[i]}`)
@@ -92,13 +87,24 @@ function createQ() {
                         clickState = true
                         answers.classList.remove('clicked')
                         answers.classList.add('clicked')
+                        if (answers.innerText[0] == currentQuestion[6]) {
+                            correctAnswers++
+                        }
                         setTimeout(() => {
-                            answersDiv.remove()
                             createQuestion.remove()
+                            answersDiv.remove()
                             ++currentNumber
-                        }, 1000);
+                            if (currentNumber >= 0 && currentNumber < 10) {
+                                createQ()
+                                answerText.forEach(textA => {
+                                    textA.remove()
+                                })
+                            } else if (currentNumber == 10) {
+                                generateRestart()
+                            }
+                        }, 700);
                     } else if (clickState == true) {
-                        console.log("you can't")
+                        alert("You can't click two buttons at once")
                     }
                 })
             })
@@ -114,8 +120,7 @@ function createQ() {
             answersDiv.appendChild(answer3)
             answersDiv.appendChild(answer4)
 
-        } else if (checkAnswer() == 2) {
-
+        } else if (checkAnswer() == "trueorfalse") {
             createQuestion.classList.add('question')
             createQuestion.innerText = `${ask}`
 
@@ -123,17 +128,16 @@ function createQ() {
 
             let answer1 = document.createElement('div')
             let answer2 = document.createElement('div')
-            let answerText1 = document.createElement('h3')
-            let answerText2 = document.createElement('h3')
 
             answersDiv.classList.add('divAnswers')
             answer1.classList.add('answers')
             answer2.classList.add('answers')
+            answer1.classList.add('true')
+            answer2.classList.add('false')
             answer1.classList.add('divButtonTemplate')
             answer2.classList.add('divButtonTemplate')
 
             let answer = [answer1, answer2]
-            let answerText = [answerText1, answerText2]
 
             answer1.innerText = "True"
             answer2.innerText = "False"
@@ -143,60 +147,62 @@ function createQ() {
                         clickState = true
                         answers.classList.remove('.clicked')
                         answers.classList.add('clicked')
-                        setTimeout(() => {
-                            answersDiv.remove()
-                            createQuestion.remove()
-                            answerText.forEach(text => {
-                                text.remove()
-                            })
-                            ++currentNumber
-                        }, 1000);
-                        if (answer1.textContent[0] == currentQuestion[6]) {
+                        if (answers.innerText == currentQuestion[4]) {
                             correctAnswers++
-                            console.log(correctAnswers)
                         }
+                        setTimeout(() => {
+                            createQuestion.remove()
+                            answersDiv.remove()
+                            ++currentNumber
+                            if (currentNumber >= 0 && currentNumber < 10) {
+                                createQ()
+                            } else if (currentNumber == 10) {
+                                generateRestart()
+                            }
+                        }, 700);
                     } else if (clickState == true) {
-                        console.log("you can't")
+                        alert("You can't click two buttons at once")
                     }
                 })
             })
             body.appendChild(createQuestion)
-            body.appendChild(answerText1)
-            body.appendChild(answerText2)
             body.appendChild(answersDiv)
             answersDiv.appendChild(answer1)
             answersDiv.appendChild(answer2)
-        } else {
-            console.log("error")
         }
-        console.log(currentNumber)
-        console.log(currentQuestion)
-        if (currentNumber == 10) {
+    }
+}
 
-            setTimeout(() => {
-                var restartBtn = document.createElement('div')
-                restartBtn.classList.add('coolButtonTemplate')
-                restartBtn.innerText = "Restart Quiz"
-                body.appendChild(restartBtn)
-                restartBtn.addEventListener('click', () => {
-                    restartBtn.classList.add('clicked')
-                    setTimeout(() => {
-                        location.reload()
-                    }, 200)
-                })
-            }, 1000);
-        }
+function generateRestart() {
+    if (currentNumber == 10) {
+
+        setTimeout(() => {
+
+            var score = document.createElement('h2')
+            score.classList.add('turnOnVisible')
+            score.innerText = `You got ${correctAnswers} of 10 answers right`
+
+            var restartBtn = document.createElement('div')
+            restartBtn.classList.add('coolButtonTemplate')
+            restartBtn.innerText = "Restart Quiz"
+
+            body.appendChild(score)
+            body.appendChild(restartBtn)
+            restartBtn.addEventListener('click', () => {
+                restartBtn.classList.add('clicked')
+                setTimeout(() => {
+                    location.reload()
+                }, 200)
+            })
+        }, 250);
+
     }
 }
 
 startQuizButton.addEventListener('click', () => {
     startQuizButton.classList.add('clicked')
     setTimeout(() => {
-        credits.classList.add('clicked')
-    }, 120)
-    setTimeout(() => {
+        divStart.remove()
         createQ()
-        credits.remove()
-        startQuizButton.remove()
-    }, 400);
+    }, 700);
 })
